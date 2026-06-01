e556e44e429dfc1c24e3a70344f59b2b3828a298
purim-sudo <128815330+purim-sudo@users.noreply.github.com>
2026-06-01 14:02:52 +0300

    Expand SecureShare API

--- SecureShare/server.js
+++ SecureShare/server.js
@@ -1,19 +1,35 @@
 const express = require('express');
+const crypto = require('crypto');
 const app = express();
 
 app.use(express.json());
 
+const uploads = [];
+
 app.get('/', (req, res) => {
   res.json({
     project: 'SecureShare',
-    status: 'online'
+    status: 'online',
+    version: '1.0.0'
   });
 });
 
 app.post('/upload', (req, res) => {
-  res.json({
-    message: 'Upload endpoint initialized'
+  const id = crypto.randomUUID();
+
+  uploads.push({
+    id,
+    created: new Date()
   });
+
+  res.json({
+    message: 'Upload registered',
+    file_id: id
+  });
+});
+
+app.get('/files', (req, res) => {
+  res.json(uploads);
 });
 
 app.listen(3000, () => {
