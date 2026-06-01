ca42aca71a948d16eb7530d314ebe684de740ec3
purim-sudo <128815330+purim-sudo@users.noreply.github.com>
2026-06-01 14:03:08 +0300

    Expand NetWatch scanner

--- NetWatch/scanner.py
+++ NetWatch/scanner.py
@@ -7,3 +7,18 @@ ip_address = socket.gethostbyname(hostname)
 
 print(f'Hostname: {hostname}')
 print(f'Local IP: {ip_address}')
+
+ports = [22, 80, 443, 3306]
+
+for port in ports:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(1)
+
+    result = sock.connect_ex((ip_address, port))
+
+    if result == 0:
+        print(f'Port {port} is open')
+    else:
+        print(f'Port {port} is closed')
+
+    sock.close()
